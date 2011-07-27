741ecb62fcef6dce23a9f19be8d4c49b3ff84992
ildyria <bvwebmaster3000-bn@yahoo.fr>
2011-07-27 16:10:40 +0200

    blabla

--- Mage analyser/includes/footer.php
+++ Mage analyser/includes/footer.php
@@ -4,6 +4,7 @@ DEFINED('MA') or die('HACKING ATTEMPT!');
 
 echo '<div style="text-align: center; padding-top: 15px;">'."\n";
 echo "\t".'<span class="c0" style="font-weight: bold;">&copy;2010-2011 Ildyria</span><br />'."\n";
+echo "\t".'<span class="c0">Source code and change log are available <a href="https://github.com/ildyria/Mage-analyser">here</a></span><br />'."\n";
 echo '</div>'."\n";
 echo '</div>'."\n";
 echo '</body>'."\n";
